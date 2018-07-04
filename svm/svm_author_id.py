a2d11f693c4b096bb0da258289a532d3651df9f7
FernandezG <gracia.fernandez@vodafone.com>
2018-07-04 12:12:01 +0100

    Lesson 3: SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -18,13 +18,21 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.svm import SVC
+C = 10000
+clf = SVC(kernel='rbf', C=C)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print(accuracy)
+import collections
+print(collections.Counter(pred))
 #########################################################
 
 
