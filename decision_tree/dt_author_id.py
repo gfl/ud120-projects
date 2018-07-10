7b19a690996b7dc343fa32844bd1971213328443
FernandezG <gracia.fernandez@vodafone.com>
2018-07-10 17:32:18 +0100

    Lesson 4: Decission trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,12 +20,18 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+print "no. of features:", len(features_train[0])
 
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print(accuracy)
 #########################################################
 
 
